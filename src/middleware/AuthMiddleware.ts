450d87e55c27916fd11ed3e518ec7447b8bbdd17
SaddamAnnais <saddamannaiss@gmail.com>
2023-11-09 13:38:06 +0700

    feat: storing payload on each requests

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { AuthToken } from "../type/auth";
 
 export class AuthMiddleware {
   async authenticate(req: Request, res: Response, next: NextFunction) {
@@ -11,6 +12,9 @@ export class AuthMiddleware {
 
     try {
       jwt.verify(token, process.env.SECRET_ACCESS_TOKEN);
+      const payload : AuthToken= JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+      res.locals.id = payload.id;
+      res.locals.isAdmin = payload.isAdmin;
       next();
     } catch (err) {
       res.clearCookie("token");
