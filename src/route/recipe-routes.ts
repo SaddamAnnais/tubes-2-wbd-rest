995d091f63685d7860ec55f3c1a92e9841736b6d
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-11-10 00:12:02 +0700

    feat: recipe routes init

--- src/route/recipe-routes.ts
+++ src/route/recipe-routes.ts
@@ -1,4 +1,7 @@
+import { NextFunction, Request, Response } from "express";
 import { RecipeController } from "../controller/RecipeController";
+import { AuthMiddleware } from "../middleware/AuthMiddleware";
+import { FileMiddleware } from "../middleware/FileMiddleware";
 import { Route } from "../type/route";
 
 // example of routes
@@ -8,11 +11,17 @@ import { Route } from "../type/route";
 // controller: controller class that manage the corresdonging request. Example: UserController,
 // action: the  method's name inside the controller class that manage the corresdonging request.
 //         Example: "all" meaning it will call the all method inside the UserController class.
+const authMiddleware = new AuthMiddleware();
 
 export const RecipeRoutes: Route[] = [
   {
     method: "post",
     route: "/recipe",
+    middleware: [
+      (req: Request, res: Response, next: NextFunction) =>
+        authMiddleware.authenticate(req, res, next),
+      FileMiddleware.upload_video_image(),
+    ],
     controller: RecipeController,
     action: "create",
   },
